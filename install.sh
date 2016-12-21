cced22bc60b05c3738249d1efbab7e2d09dc915c
Alexey Kuzmin <Aleksej.Kyzmin@gmail.com>
2016-12-21 23:09:28 +0300

    Правки в install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+sudo rm /etc/nginx/sites-available/default
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo rm /etc/nginx/sites-enabled/default
 
 cd /home/box/web
-gunicorn -w 1 -b 0.0.0.0:8080 hello:app &
+gunicorn -w 1 -b 0.0.0.0:8080 hello:app
