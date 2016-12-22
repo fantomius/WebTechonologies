e8ff41af38073addc8ee9cb29b25a12c9f171f6b
Alexey Kuzmin <Aleksej.Kyzmin@gmail.com>
2016-12-23 01:08:23 +0300

    Added django project

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 cd /home/box/web
-gunicorn -w 1 -b 0.0.0.0:8080 hello:app
+gunicorn -w 1 -b 0.0.0.0:8080 hello:app &
+gunicorn -w 1 -b 0.0.0.0:8000 ask.ask.wsgi:application
