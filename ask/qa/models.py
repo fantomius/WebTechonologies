4b38a8ddd3ddb9ac8056c77a4735aa4ac4ec9e42
Alexey Kuzmin <Aleksej.Kyzmin@gmail.com>
2016-12-24 19:46:36 +0300

    Добавил работу с db

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateTimeField()
+	rating = models.FloatField()
+	author = models.ForeignKey(User, on_delete=models.SET_NULL)
+	links = models.ManyToManyField(User)
+
+	class Meta:
+		db_table = 'qa_question'
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField()
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	author = models.ForeignKey(User, on_delete=models.SET_NULL)
+
+	class Meta:
+		db_table = 'qa_answer'
+
+class QuestionManager(models.Manager):
+	def new(self, count=10):
+		return super(QuestionManager, self).get_queryset().order_buy("-added_at")[:count]
+
+	def popular(self):
+		return super(QuestionManager, self).get_queryset().order_buy("-rating")
